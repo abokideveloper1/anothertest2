88f9576700c0347a41d26fef312b944b1bf216a2
abokideveloper1 <xandykelvin@gmail.com>
2020-07-21 21:40:28 -0300

    Good afternoon,Good evening and Good night sentences had been added!

--- src/anothertest2/mainClass.java
+++ src/anothertest2/mainClass.java
@@ -4,7 +4,10 @@ public class mainClass {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Good morning");
+		System.out.println("Good morning!");
+		System.out.println("Good afternoon!");
+		System.out.println("Good evening!");
+		System.out.println("Good night!");
 
 	}
 
